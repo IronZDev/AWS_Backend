d0a2fb886fd620895e72463eb7e6f56bf8774420
IronZaphilith <maciek_st@o2.pl>
2020-09-04 15:52:45 +0200

    Fixed presigned POST

--- app.js
+++ app.js
@@ -3,45 +3,48 @@ const app = express();
 const port = 5000;
 
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY, // aws access id here
-  secretAccessKey: process.env.AWS_SECRET, // aws secret access key here
-  sessionToken: process.env.AWS_SESSION_TOKEN,
-  useAccelerateEndpoint: false,
-  region: 'us-east-1',
-});
-const params = {
-  Bucket: "mstokfisz-to-process",
-  Key: '',
-  Expires: 60*60, // expiry time 1h
-  ACL: "private",
-  ContentType: "image/jpeg", // accept any image
-};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // api endpoint to get signed url
-app.post("/get-signed-url", (req, res) => {
-  console.log(req);
-  params.Key = req.body.filename;
-  s3.getSignedUrl("putObject", params, function(err, url) {
-    if (err) {
-      console.log("Error getting presigned url from AWS S3");
-      res.json({
-        success: false,
-        message: "Pre-Signed URL error",
-        url: url
-      });
-    } else {
-      console.log("Presigned URL: ", url);
-      res.json({
-        success: true,
-        message: "AWS SDK S3 Pre-signed url generated successfully.",
-        url: url
-      });
-    }
+
+app.post('/signed-form-upload', async (req, res) => {
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY, // aws access id here
+    secretAccessKey: process.env.AWS_SECRET, // aws secret access key here
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+    region: 'us-east-1',
+    signatureVersion: 'v4',
   });
+  const params = {
+    Bucket: 'images-to-process-mstokfisz',
+    Key: req.body.filename,
+    Fields: {
+      Key: req.body.filename,
+      'X-Amz-Meta-Rotation': '1',
+      'success_action_status': '201'
+    },
+  };
+  const options = {
+    signatureVersion: 'v4',
+    region: 'us-east-1', // same as your bucket
+    endpoint: new AWS.Endpoint('https://images-to-process-mstokfisz.s3.amazonaws.com'),
+    useAccelerateEndpoint: false,
+    s3ForcePathStyle: true,  }
+
+  const client = new AWS.S3(options);
+  const form = await (new Promise((resolve, reject) => {
+
+    client.createPresignedPost(params, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    });
+  }));
+  return { ...res.json(form), url: options.endpoint.host }
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
