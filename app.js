0428889b081c0adcac4cf2823f798f0c4ba0dda1
IronZaphilith <maciek_st@o2.pl>
2020-09-04 13:23:12 +0200

    Added session token

--- app.js
+++ app.js
@@ -4,36 +4,41 @@ const port = 5000;
 
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3({
-  accessKeyId: "ASIA4V76QNYSIUIOK4WH", // aws access id here
+  accessKeyId: process.env.AWS_ACCESS_KEY, // aws access id here
   secretAccessKey: process.env.AWS_SECRET, // aws secret access key here
-  useAccelerateEndpoint: true
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+  useAccelerateEndpoint: false,
+  region: 'us-east-1',
 });
 const params = {
   Bucket: "mstokfisz-to-process",
-  Key: "ASIA4V76QNYSIUIOK4WH",
-  Expires: 60*60, // expiry time
-  ACL: "bucket-owner-full-control",
-  ContentType: "image/jpeg" // this can be changed as per the file type
+  Key: '',
+  Expires: 60*60, // expiry time 1h
+  ACL: "private",
+  ContentType: "image/jpeg", // accept any image
 };
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // api endpoint to get signed url
-app.get("/get-signed-url", (req, res) => {
-  const fileurls = [];
+app.post("/get-signed-url", (req, res) => {
+  console.log(req);
+  params.Key = req.body.filename;
   s3.getSignedUrl("putObject", params, function(err, url) {
     if (err) {
       console.log("Error getting presigned url from AWS S3");
       res.json({
         success: false,
         message: "Pre-Signed URL error",
-        urls: fileurls
+        url: url
       });
     } else {
-      fileurls[0] = url;
-      console.log("Presigned URL: ", fileurls[0]);
+      console.log("Presigned URL: ", url);
       res.json({
         success: true,
-        message: "AWS SDK S3 Pre-signed urls generated successfully.",
-        urls: fileurls
+        message: "AWS SDK S3 Pre-signed url generated successfully.",
+        url: url
       });
     }
   });
